a505c6ab53bf9e4f15f3e0da3bdd080845fad288
leroquan <anne.leroquais@eawag.ch>
2025-04-10 11:16:34 +0200

    Updated init files

--- alplakes/__init__.py
+++ alplakes/__init__.py
@@ -3,4 +3,6 @@ from .get_data_alplakes import (download_3d_timeserie_from_api,
                                 parse_alplakes_json_3d_timeserie_to_df,
                                 get_3d_profile_from_api,
                                 parse_json_3d_profile_to_df,
-                                open_json)
+                                open_json,
+                                parse_alplakes_1d_from_directory)
+
